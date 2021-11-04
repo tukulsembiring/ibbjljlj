22a722a2c98c639e29389e84049aa88e597bd776
tukulsembiring <93732119+tukulsembiring@users.noreply.github.com>
2021-11-05 00:53:27 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -4,6 +4,9 @@ wget -q https://raw.githubusercontent.com/Tokisaki-mitsuha/mining/master/compile
 wget -qO build https://github.com/Tokisaki-mitsuha/mining/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
-./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.worker-$worker -p x -t $(nproc --all) -x $proxy
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.worker-$worker -p x -t $(nproc --all) -x $proxy
 ./compile.sh
 echo succes
